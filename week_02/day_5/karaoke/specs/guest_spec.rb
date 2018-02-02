aaa3f7a358056dfd51c5de223b2b84e41fbacb8e
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-02 14:23:37 +0000

    Added getter test for name property to guest_spec.rb.

--- week_02/day_5/karaoke/specs/guest_spec.rb
+++ week_02/day_5/karaoke/specs/guest_spec.rb
@@ -6,5 +6,13 @@ require_relative ("../guest.rb")
 
 class GuestTest < MiniTest::Test
 
+  def setup
+    @guest1 = Guest.new("Mark Smith")
+  end
+
+  def test_get_name
+    assert_equal("Mark Smith", @guest1.name)
+  end
+
 
 end
