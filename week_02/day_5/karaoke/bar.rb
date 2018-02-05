cab4b9aceba45fa6c30a7efe3104b1947b51cc94
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-05 07:50:41 +0000

    Addedadd_room and count_rooms to Bar class, plus TDD tests.

--- week_02/day_5/karaoke/bar.rb
+++ week_02/day_5/karaoke/bar.rb
@@ -6,6 +6,15 @@ class Bar
 
   def initialize(name)
     @name = name
+    @rooms = []
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+
+  def count_rooms
+    return @rooms.count()
   end
 
 end
--- week_02/day_5/karaoke/specs/bar_spec.rb
+++ week_02/day_5/karaoke/specs/bar_spec.rb
@@ -3,15 +3,31 @@ require ("minitest/rg")
 require ("pry")
 
 require_relative ("../bar.rb")
+require_relative ("../room.rb")
 
 class BarTest < MiniTest::Test
 
   def setup
-    @bar = Bar.new("HotShots")
+    @bar1 = Bar.new("HotShots")
+    @room1 = Room.new("70's Disco", 20)
+    @room2 = Room.new("80's Electric", 20)
+    @room3 = Room.new("90's Indie", 20)
   end
 
   def test_get_bar_name
-    assert_equal("HotShots", @bar.name)
+    assert_equal("HotShots", @bar1.name)
+  end
+
+  def test_add_room__1_room
+    @bar1.add_room(@room1)
+    assert_equal(1, @bar1.count_rooms())
+  end
+
+  def test_add_room__3_rooms
+    @bar1.add_room(@room1)
+    @bar1.add_room(@room2)
+    @bar1.add_room(@room3)
+    assert_equal(3, @bar1.count_rooms())
   end
 
 end
