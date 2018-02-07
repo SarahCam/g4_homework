570a00597a4bda53419f440b238417b0b41f252d
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-07 15:20:03 +0000

    Added run method in a SqlRunner class, that can be reused by other classes to run SQL queries against the music_collection database.  Added a new save method to the Artist class, to save the artist details into the music_collection database, using the run method now available. Added a line to console.rb to save the artist to the database, so we can check it works.

--- week_03/day_3/music_collection/console.rb
+++ week_03/day_3/music_collection/console.rb
@@ -3,6 +3,8 @@ require_relative('models/artist')
 require_relative('models/album')
 
 artist1 = Artist.new( {'name' => 'Lady Gaga'} )
+artist1.save()
+
 album1 = Album.new( {
   'name' => 'Artpop',
   'genre' => 'pop'
--- week_03/day_3/music_collection/models/artist.rb
+++ week_03/day_3/music_collection/models/artist.rb
@@ -1,4 +1,5 @@
 require('pry')
+require_relative('../db/sql_runner')
 
 class Artist
 
@@ -7,6 +8,13 @@ class Artist
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @name = options['name']
+  end
+
+  def save()
+    sql = "INSERT INTO artists (name) VALUES ($1) RETURNING id"
+    values = [@name]
+    save = SqlRunner.run(sql, values)
+    @id = save[0]["id"].to_i
   end
 
 end
