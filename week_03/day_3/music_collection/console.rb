1c04319d64adfa20e5006df4d20b581ed68a86fc
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-07 14:59:30 +0000

    Added an instance of the Album class to console.rb.

--- week_03/day_3/music_collection/console.rb
+++ week_03/day_3/music_collection/console.rb
@@ -3,6 +3,10 @@ require_relative('models/artist')
 require_relative('models/album')
 
 artist1 = Artist.new( {'name' => 'Lady Gaga'} )
+album1 = Album.new( {
+  'name' => 'Artpop',
+  'genre' => 'pop'
+  } )
 
 binding.pry
 nil
