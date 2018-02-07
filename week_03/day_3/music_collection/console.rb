6bbe359eeab2237f3cf59a98ef1ecd04baabf43f
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-07 16:18:18 +0000

    Added another artist and some more albums. Also changed the order of my delete_all methods, so that Albums are deleted first, then Artists - to avoid a foreign key constraint issue.

--- week_03/day_3/music_collection/console.rb
+++ week_03/day_3/music_collection/console.rb
@@ -4,23 +4,47 @@ require_relative('models/album')
 
 ### EMPTY ALL DATABASE TABLES ############################################
 
-Artist.delete_all()
 Album.delete_all()
+Artist.delete_all()
 
 ### ADD NEW ARTIST & SAVE TO DB ##########################################
 
 artist1 = Artist.new( {'name' => 'Lady Gaga'} )
+artist2 = Artist.new( {'name' => 'Depeche Mode'} )
+
 artist1.save()
+artist2.save()
 
 ### ADD NEW ALBUM & SAVE TO DB ###########################################
 
 album1 = Album.new( {
   'name' => 'Artpop',
-  'genre' => 'pop',
+  'genre' => 'Pop',
   'artist_id' => artist1.id
   } )
 
+album2 = Album.new( {
+  'name' => 'Speak and Spell',
+  'genre' => 'Indie',
+  'artist_id' => artist2.id
+  } )
+
+album3 = Album.new( {
+  'name' => 'Black Celebration',
+  'genre' => 'Indie',
+  'artist_id' => artist2.id
+  } )
+
+album4 = Album.new( {
+  'name' => 'Music for the Masses',
+  'genre' => 'Indie',
+  'artist_id' => artist2.id
+  } )
+
 album1.save()
+album2.save()
+album3.save()
+album4.save()
 
 ### DEBUGGING ############################################################
 
