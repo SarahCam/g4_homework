49d657b901870350eeb61a8f94bcda50cab4b7c0
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-07 14:53:44 +0000

    Added name property to Album class and albums table. Also updated console.db so that the options passed into creating a new artist instance were in a {} hash.

--- week_03/day_3/music_collection/console.rb
+++ week_03/day_3/music_collection/console.rb
@@ -2,7 +2,7 @@ require('pry')
 require_relative('models/artist')
 require_relative('models/album')
 
-artist1 = Artist.new('name' => 'Lady Gaga')
+artist1 = Artist.new( {'name' => 'Lady Gaga'} )
 
 binding.pry
 nil
--- week_03/day_3/music_collection/models/album.rb
+++ week_03/day_3/music_collection/models/album.rb
@@ -2,10 +2,11 @@ require('pry')
 
 class Album
 
-  attr_reader :id, :genre, :artist_id
+  attr_reader :id, :name, :genre, :artist_id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
+    @name = options['name']
     @genre = options['genre']
     @artist_id = options['artist_id'].to_i
   end
