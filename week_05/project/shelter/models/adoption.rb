06e685f95f6937db17fd71539956b6fac50895bb
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-19 18:57:09 +0000

    Added save method plus TDD for Adoptions class.

---
+++ week_05/project/shelter/models/adoption.rb
@@ -0,0 +1,73 @@
+require('pry')
+require_relative('../db/sql_runner')
+
+class Adoption
+
+  attr_reader :id
+  attr_accessor :animal_id, :owner_id, :adoption_date
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @animal_id = options['animal_id']
+    @owner_id = options['owner_id']
+    @adoption_date = options['adoption_date']
+  end
+
+  def save()
+    sql = "INSERT INTO adoptions (animal_id, owner_id, adoption_date)
+           VALUES ($1, $2, $3)
+           RETURNING id"
+    values = [@animal_id, @owner_id, @adoption_date]
+    save = SqlRunner.run(sql, values)
+    @id = save.first()['id'].to_i
+  end
+
+  # def delete()
+  #   sql = "DELETE FROM owners WHERE id = $1"
+  #   values = [@id]
+  #   SqlRunner.run(sql,values)
+  # end
+  #
+  # def update()
+  #   sql = "UPDATE owners
+  #          SET first_name = $1,
+  #              last_name = $2,
+  #              telephone = $3,
+  #              email = $4,
+  #              address = $5,
+  #              postcode = $6,
+  #              species = $7,
+  #              breed = $8,
+  #              seeks_pet = $9,
+  #              registration_date = $10
+  #          WHERE id = $11"
+  #   values = [@first_name, @last_name, @telephone, @email, @address, @postcode, @species, @breed, @seeks_pet, @registration_date, @id]
+  #   SqlRunner.run(sql,values)
+  # end
+  #
+  # def self.find_by_id(id)
+  #   sql = "SELECT * FROM owners WHERE id = $1"
+  #   values = [id]
+  #   result = SqlRunner.run(sql, values).first()
+  #   return Owner.new(result)
+  # end
+  #
+  # def self.find_by_first_name(first_name)
+  #   sql = "SELECT * FROM owners WHERE first_name = $1"
+  #   values = [first_name]
+  #   result = SqlRunner.run(sql, values).first()
+  #   return Owner.new(result)
+  # end
+  #
+  # def self.find_all()
+  #   sql = "SELECT * FROM owners"
+  #   results = SqlRunner.run(sql)
+  #   return results.map { |result| Owner.new(result) }
+  # end
+  #
+  # def self.delete_all()
+  #   sql = "DELETE FROM owners"
+  #   SqlRunner.run(sql)
+  # end
+
+end
---
+++ week_05/project/shelter/specs/adoption_spec.rb
@@ -0,0 +1,154 @@
+require('minitest/autorun')
+require('minitest/rg')
+require('pry')
+require('date')
+
+require_relative('../models/owner')
+require_relative('../models/animal')
+require_relative('../models/adoption')
+
+
+class AdoptionTest < MiniTest::Test
+
+  def setup
+
+    # DELETE ANIMALS
+    Animal.delete_all()
+
+    # CREATE ANIMALS
+    @animal_1 = Animal.new({ "name" => "Harry",
+                            "species" => "Dog",
+                            "breed" => "Mongrel",
+                            "gender" => "Male",
+                            "age" => 7,
+                            "photo" => "harry.jpg",
+                            "healthy" => "Yes",
+                            "safe" => "No",
+                            "adopted" => false,
+                            "admission_date" => DateTime.new(2018,1,22).to_s
+                          })
+    @animal_1.save()
+    @animal_2 = Animal.new({ "name" => "Mog",
+                            "species" => "Cat",
+                            "breed" => "Tabby",
+                            "gender" => "Female",
+                            "age" => 4,
+                            "photo" => "mog.jpg",
+                            "healthy" => "No",
+                            "safe" => "Yes",
+                            "adopted" => false,
+                            "admission_date" => DateTime.new(2018,2,3).to_s
+                          })
+    @animal_2.save()
+    @animal_3 = Animal.new({ "name" => "Blue",
+                            "species_id" => "Dog",
+                            # "breed" => "Alsation",
+                            # "gender" => "Male",
+                            # "age" => 2,
+                            # "photo" => "mog.jpg",
+                            "healthy" => "No",
+                            "safe" => "Check",
+                            "adopted" => false,
+                            "admission_date" => DateTime.new(2018,2,11).to_s
+                          })
+    @animal_3.save()
+
+    # DELETE OWNERS
+    Owner.delete_all()
+
+    # CREATE OWNERS
+    @owner_1 = Owner.new({  "first_name" => "Bill",
+                            "last_name" => "Wardlaw",
+                            "telephone" => "01786 832914",
+                            "email" => "bill.wardlaw@gmail.com",
+                            "address" => "11 PistolMakers Row",
+                            "postcode" => "FK16 7RB",
+                            "species" => "Dog",
+                            "breed" => "No preference",
+                            "seeks_pet" => true,
+                            "registration_date" => DateTime.new(2018,2,18).to_s
+                          })
+    @owner_1.save()
+    @owner_2 = Owner.new({  "first_name" => "Jemima",
+                            "last_name" => "Price",
+                            "telephone" => "01786 855294",
+                            "email" => "jemima.price@btinternet.com",
+                            "address" => "20 Main Street",
+                            "postcode" => "FK16 8FD",
+                            "species" => "Cat",
+                            "breed" => "Tabby",
+                            "seeks_pet" => true,
+                            "registration_date" => DateTime.new(2017,12,30).to_s
+                          })
+    @owner_2.save()
+    @owner_3 = Owner.new({  "first_name" => "Gavin",
+                            "last_name" => "Templeton",
+                            "telephone" => "01786 841050",
+                            # "email" => "gavin@ideas.com",
+                            # "address" => "???",
+                            # "postcode" => "???",
+                            "species" => "Snake",
+                            # "breed" => "No preference",
+                            "seeks_pet" => true,
+                            "registration_date" => DateTime.new(2018,1,3).to_s
+                          })
+    @owner_3.save()
+
+    # DELETE ADOPTIONS
+    # Adoption.delete_all()
+
+    @adoption_1 = Adoption.new({  "animal_id" => @animal_1.id,
+                                  "owner_id" => @owner_1.id,
+                                  "adoption_date" => DateTime.new(2018,2,14).to_s
+                          })
+    @adoption_1.save()
+
+  end
+
+  def test_get_date
+    assert_equal(DateTime.new(2018,2,14).to_s, @adoption_1.adoption_date)
+  end
+
+  # def test_find_all___3
+  #   assert_equal(3, Owner.find_all().count())
+  # end
+  #
+  # def test_find_by_id___Bill
+  #   assert_equal("Bill", Owner.find_by_id(@owner_1.id).first_name)
+  # end
+  #
+  # def test_find_by_first_name___Bill
+  #   assert_equal("Bill", Owner.find_by_first_name(@owner_1.first_name).first_name)
+  # end
+  #
+  # def test_delete___2
+  #   @owner_3.delete()
+  #   assert_equal(2, Owner.find_all().count())
+  # end
+  #
+  # def test_update__first_name___Charlie
+  #   @owner_2.first_name = "Charlie"
+  #   @owner_2.update()
+  #   assert_equal("Charlie", Owner.find_by_id(@owner_2.id).first_name)
+  # end
+  #
+  # def test_update__species___Snake
+  #   @owner_2.species = "Snake"
+  #   @owner_2.update()
+  #   assert_equal("Snake", Owner.find_by_id(@owner_2.id).species)
+  # end
+
+  # def test_update__gender___Moggy
+  #   @animal_2.gender = "Male"
+  #   @animal_2.update()
+  #   assert_equal("Male", Animal.find_by_id(@animal_2.id).gender)
+  # end
+
+  # def test_update__admission_date___Moggy
+  #   new_date = DateTime.new(2018,1,10).to_s
+  #   @animal_2.admission_date = new_date
+  #   @animal_2.update()
+  #   assert_equal(new_date, Animal.find_by_id(@animal_2.id).admission_date)
+  # end
+
+end
