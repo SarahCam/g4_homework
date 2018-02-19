cedb9e0885557581c33e7f7d6246a20f5037f527
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-19 10:50:44 +0000

    Added CRUD methods to Animal class, in a TDD manner, by adding tests first to the animal_spec.rb file, to do the the Red Green Refactor approach.

--- week_05/project/shelter/models/animal.rb
+++ week_05/project/shelter/models/animal.rb
@@ -29,6 +29,49 @@ class Animal
     @id = save.first()['id'].to_i
   end
 
+  def delete()
+    sql = "DELETE FROM animals WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql,values)
+  end
+
+  def update()
+    sql = "UPDATE animals
+           SET name = $1,
+               species_id = $2,
+               breed_id = $3,
+               gender = $4,
+               age = $5,
+               photo = $6,
+               healthy = $7,
+               safe = $8,
+               adopted = $9,
+               admission_date = $10
+           WHERE id = $11"
+    values = [@name, @species_id, @breed_id, @gender, @age, @photo, @healthy, @safe, @adopted, @admission_date, @id]
+    SqlRunner.run(sql,values)
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM animals WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first()
+    return Animal.new(result)
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM animals WHERE name = $1"
+    values = [name]
+    result = SqlRunner.run(sql, values).first()
+    return Animal.new(result)
+  end
+
+  def self.find_all()
+    sql = "SELECT * FROM animals"
+    results = SqlRunner.run(sql)
+    return results.map { |result| Animal.new(result) }
+  end
+
   def self.delete_all()
     sql = "DELETE FROM animals"
     SqlRunner.run(sql)
--- week_05/project/shelter/specs/animal_spec.rb
+++ week_05/project/shelter/specs/animal_spec.rb
@@ -11,28 +11,127 @@ require_relative('../models/animal')
 class AnimalTest < MiniTest::Test
 
   def setup
-    @dog_species = Species.new({ "name" => "dog" })
 
-    @dog_breed_1 = Breed.new({ "species_id" => @dog_species.id, "name" => "mongrel"})
-    @dog_breed_2 = Breed.new({ "species_id" => @dog_species.id, "name" => "collie"})
-    @dog_breed_3 = Breed.new({ "species_id" => @dog_species.id, "name" => "labrador"})
-    @dog_breed_4 = Breed.new({ "species_id" => @dog_species.id, "name" => "spaniel"})
+    # DELETE ANIMALS, BREEDS & SPECIES
+    Animal.delete_all()
+    Breed.delete_all()
+    Species.delete_all()
 
-    @dog_1 = Animal.new ({  "name" => "Harry",
-                            "species_id" => @dog_species.id,
-                            "breed_id" => @dog_breed_1.id,
+    # CREATE & SAVE ANIMAL SPECIES
+    @dog = Species.new ({ 'name' => 'dog' })
+    @dog.save()
+    @cat = Species.new ({ 'name' => 'cat' })
+    @cat.save()
+    @rabbit = Species.new ({ 'name' => 'rabbit' })
+    @rabbit.save()
+    @snake = Species.new ({ 'name' => 'snake' })
+    @snake.save()
+    @bird = Species.new ({ 'name' => 'bird' })
+    @bird.save()
+
+    # CREATE & SAVE DOG BREEDS
+    @dog_1 = Breed.new({ "species_id" => @dog.id, "name" => "mongrel"})
+    @dog_1.save()
+    @dog_2 = Breed.new({ "species_id" => @dog.id, "name" => "collie"})
+    @dog_2.save()
+    @dog_3 = Breed.new({ "species_id" => @dog.id, "name" => "labrador"})
+    @dog_3.save()
+    @dog_4 = Breed.new({ "species_id" => @dog.id, "name" => "spaniel"})
+    @dog_4.save()
+
+    # CREATE & SAVE CAT BREEDS
+    @cat_1 = Breed.new({ "species_id" => @cat.id, "name" => "tabby"})
+    @cat_1.save()
+    @cat_2 = Breed.new({ "species_id" => @cat.id, "name" => "siamese"})
+    @cat_2.save()
+    @cat_3 = Breed.new({ "species_id" => @cat.id, "name" => "black"})
+    @cat_3.save()
+    @cat_4 = Breed.new({ "species_id" => @cat.id, "name" => "tortoiseshell"})
+    @cat_4.save()
+
+    # CREATE ANIMALS
+    @animal_1 = Animal.new({ "name" => "Harry",
+                            "species_id" => @dog.id,
+                            "breed_id" => @dog_1.id,
                             "gender" => "M",
                             "age" => 7,
                             "photo" => "harry.jpg",
                             "healthy" => "Y",
                             "safe" => "N",
                             "adopted" => "N",
-                            "admission_date" => DateTime.new(2018,1,22,9,30)
-                        })
+                            "admission_date" => DateTime.new(2018,1,22).to_s
+                          })
+    @animal_1.save()
+    @animal_2 = Animal.new({ "name" => "Mog",
+                            "species_id" => @cat.id,
+                            "breed_id" => @cat_1.id,
+                            "gender" => "F",
+                            "age" => 4,
+                            "photo" => "mog.jpg",
+                            "healthy" => "N",
+                            "safe" => "Y",
+                            "adopted" => "N",
+                            "admission_date" => DateTime.new(2018,2,3).to_s
+                          })
+    @animal_2.save()
+    @animal_3 = Animal.new({ "name" => "Blue",
+                            "species_id" => @dog.id,
+                            # "breed_id" => @dog_1.id,
+                            "gender" => "M",
+                            # "age" => 2,
+                            # "photo" => "mog.jpg",
+                            # "healthy" => "N",
+                            # "safe" => "Y",
+                            "adopted" => "N",
+                            "admission_date" => DateTime.new(2018,2,11).to_s
+                          })
+    @animal_3.save()
   end
 
-  def test_get_dog_name
-    assert_equal("Harry", @dog_1.name)
+  def test_get_dog_name___Harry
+    assert_equal("Harry", @animal_1.name)
   end
+
+  def test_find_all___3
+    assert_equal(3, Animal.find_all().count())
+  end
+
+  def test_find_by_id___Mog
+    assert_equal("Mog", Animal.find_by_id(@animal_2.id).name)
+  end
+
+  def test_find_by_name___Mog
+    assert_equal("Mog", Animal.find_by_name(@animal_2.name).name)
+  end
+
+  def test_delete___2
+    @animal_3.delete()
+    assert_equal(2, Animal.find_all().count())
+  end
+
+  def test_update__name___Moggy
+    @animal_2.name = "Moggy"
+    @animal_2.update()
+    assert_equal("Moggy", Animal.find_by_id(@animal_2.id).name)
+  end
+
+  def test_update__age___Moggy
+    @animal_2.age = 10
+    @animal_2.update()
+    assert_equal(10, Animal.find_by_id(@animal_2.id).age)
+  end
+
+  def test_update__gender___Moggy
+    @animal_2.gender = "M"
+    @animal_2.update()
+    assert_equal("M", Animal.find_by_id(@animal_2.id).gender)
+  end
+
+  # def test_update__admission_date___Moggy
+  #   new_date = DateTime.new(2018,1,10).to_s
+  #   @animal_2.admission_date = new_date
+  #   @animal_2.update()
+  #   assert_equal(new_date, Animal.find_by_id(@animal_2.id).admission_date)
+  # end
 
 end
